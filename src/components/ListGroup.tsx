55ee317f9c7fd3a0bcf63cd62c3e42b2e7a31ff3
ARTMashimbyi <2562071@students.wits.ac.za>
2024-12-11 01:20:27 +0200

    dynamic list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,26 @@
 import { Fragment } from "react/jsx-runtime";
 
 function ListGroup() {
+  const items = [
+    "Benoni",
+    "Boksburg",
+    "Brakpan",
+    "Germiston",
+    "Johannesburg",
+    "Pretoria",
+    "Randburg",
+    "Randfontein",
+    "Roodepoort",
+    "Soweto",
+  ];
+
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item active" aria-current="true">
-          An active item
-        </li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
       </ul>
     </>
   );
