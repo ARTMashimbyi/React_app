310bf4f7d807a1df1df81695071dc51b3b4de3d5
ARTMashimbyi <2562071@students.wits.ac.za>
2024-12-11 00:52:53 +0200

    ListGroup

--- src/App.tsx
+++ src/App.tsx
@@ -35,11 +35,11 @@ export default App
 */
 
 
-import Message from "./Message";
+import ListGroup from ".//components/ListGroup";
 
 function App() {
 
-return <div><Message></Message></div>
+return <div><ListGroup></ListGroup></div>
 }
 
 export default App 
